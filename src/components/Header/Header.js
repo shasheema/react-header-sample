37277ffb4a1a6914505f7b654193de9c8c5dcc09
Shasheema De Noyel <shasheema.denoyel@pearson.com>
2022-03-21 01:31:16 +0530

    mobile modifications

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,28 +12,29 @@ export default class Header extends Component {
     render() {
         return (
             <React.Fragment>
-            <button type="button" id="mobile-nav-toggle"><i className="fa fa-bars"></i></button>
             <header id="header" className="header">
-                <div className="container">
-
-                <div id="logo" className="pull-left">
-                    <h1><a href="index.html"><img src="https://datostechnology.com/public_html/assets1/img/detosLogoWeb.png" id="logoImg" width="100px" /></a></h1>
-                
+                <section class="top-nav">
+                <div>
+                    <div id="logo" className="pull-left">
+                        <h1><a href="index.html"><img src="https://datostechnology.com/public_html/assets1/img/detosLogoWeb.png" id="logoImg" width="100px" /></a></h1>
+                    </div>
                 </div>
-
-                <nav id="nav-menu-container">
-                    <ul className="nav-menu sf-js-enabled sf-arrows">
+                <input id="menu-toggle" type="checkbox" />
+                <label class='menu-button-container' for="menu-toggle">
+                    <div class='menu-button'></div>
+                </label>
+                <ul class="nav-menu menu">
                     <li className="menu-active"><a href="#">Home</a></li>
-                    <li className=""><a href="#get-started">Get started</a></li>
                     <li className=""><a href="#about-us">About</a></li>
                     <li className=""><a href="#features">Services</a></li>
                     <li className=""><a href="#team">Technologies</a></li>
                     <li className=""><a href="#blog">Articles</a></li>
                     <li className=""><a href="#contact">Contact</a></li>
-                    </ul>
-                </nav>
-                </div>
+                </ul>
+            </section>
             </header>
+
+           
             </React.Fragment>
             
 
